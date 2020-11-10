1acf0452bf466098ae5801721f2b09d4b14d8ff5
Philister <1026619787@qq.com>
2020-11-10 20:09:55 +0800

    Spring quick start

---
+++ Spring/wangzhi_spring_ioc/src/main/java/com/wangzhi/dao/UserDao.java
@@ -0,0 +1,5 @@
+package com.wangzhi.dao;
+
+public interface UserDao {
+    public void save();
+}
---
+++ Spring/wangzhi_spring_ioc/src/main/java/com/wangzhi/dao/impl/UserDaoImpl.java
@@ -0,0 +1,9 @@
+package com.wangzhi.dao.impl;
+
+import com.wangzhi.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+    public void save(){
+        System.out.println("save running......");
+    }
+}
