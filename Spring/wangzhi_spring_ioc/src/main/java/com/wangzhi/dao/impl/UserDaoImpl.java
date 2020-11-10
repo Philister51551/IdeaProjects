dceda401173d967457b9cdbf0db729212cffddf6
Philister <1026619787@qq.com>
2020-11-10 21:58:59 +0800

    Bean label

--- Spring/wangzhi_spring_ioc/src/main/java/com/wangzhi/dao/impl/UserDaoImpl.java
+++ Spring/wangzhi_spring_ioc/src/main/java/com/wangzhi/dao/impl/UserDaoImpl.java
@@ -3,6 +3,19 @@ package com.wangzhi.dao.impl;
 import com.wangzhi.dao.UserDao;
 
 public class UserDaoImpl implements UserDao {
+
+    public UserDaoImpl() {
+        System.out.println("UserDaoImpl对象创建......");
+    }
+
+    public void init(){
+        System.out.println("初始化方法......");
+    }
+
+    public void destory(){
+        System.out.println("销毁方法......");
+    }
+
     public void save(){
         System.out.println("save running......");
     }
