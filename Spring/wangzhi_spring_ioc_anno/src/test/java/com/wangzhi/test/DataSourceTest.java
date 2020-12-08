0387ff3937f44b5e4cb3c0619cef8fc546acded6
Philister <1026619787@qq.com>
2020-12-08 20:12:30 +0800

    Manually create the data source

---
+++ Spring/wangzhi_spring_ioc_anno/src/test/java/com/wangzhi/test/DataSourceTest.java
@@ -0,0 +1,62 @@
+package com.wangzhi.test;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidPooledConnection;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.junit.Test;
+
+import java.beans.PropertyVetoException;
+import java.sql.Connection;
+import java.util.ResourceBundle;
+
+public class DataSourceTest {
+    @Test
+    //测试手动创建c3p0数据源（加载properties配置文件的形式）
+    public void test3() throws Exception {
+        //读取配置文件
+        ResourceBundle rb = ResourceBundle.getBundle("jdbc");
+        String driver = rb.getString("jdbc.driver");
+        String url = rb.getString("jdbc.url");
+        String username = rb.getString("jdbc.username");
+        String password = rb.getString("jdbc.password");
+        //创建数据源对象，设置连接参数
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass(driver);
+        dataSource.setJdbcUrl(url);
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+
+
+    }
+
+    @Test
+    //测试手动创建druid数据源
+    public void test2() throws Exception {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/test");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        DruidPooledConnection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+
+    }
+
+    @Test
+    //测试手动创建c3p0数据源
+    public void test1() throws Exception {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();//导数据源
+        dataSource.setDriverClass("com.mysql.jdbc.Driver");            //设置四个基本的连接参数
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/test");
+        dataSource.setUser("root");
+        dataSource.setPassword("root");
+        Connection connection = dataSource.getConnection();            //获取资源
+        System.out.println(connection);
+        connection.close();
+
+    }
+}
